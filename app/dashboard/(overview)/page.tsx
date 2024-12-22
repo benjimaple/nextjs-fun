05128d49278879107b4c9a92e24e1b4d4c797a52
Khanh <khanh.nguyen@sensen.ai>
2024-12-22 13:58:59 +1100

    finished Nextjs course

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -8,6 +8,13 @@ import {
   LatestInvoicesSkeleton,
   RevenueChartSkeleton,
 } from "@/app/ui/skeletons";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Acme Dashboard",
+  description: "The official Next.js Course Dashboard, built with App Router.",
+  metadataBase: new URL("https://next-learn-dashboard.vercel.sh"),
+};
 
 export default async function Page() {
   // const revenue = await fetchRevenue();
